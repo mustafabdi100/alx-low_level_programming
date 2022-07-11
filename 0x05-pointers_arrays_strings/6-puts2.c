792e0904ac99ab780009ff638ebb89c12953e261
mustafabdi100 <mustafa300abdi@gmail.com>
2022-07-11 11:09:41 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,41 +1,17 @@
 #include "main.h"
 
-
-
 /**
+ * puts2 - prints part of a string
+ * @str: string to be printed
  *
- *  * puts2 - prints part of a string
- *
- *   * @str: string to be printed
- *
- *    *
- *
- *     * Return: void
- *
- *      **/
-
+ * Return: void
+ **/
 void puts2(char *str)
-
 {
-
-	int i = 0;
-
-	while (str[i] != '\0')
-
-	{
-
-		if (i % 2 == 0)
-
-			_putchar(str[i]);
-
-		i++;
-
-	}
-
-
-
-	_putchar('\n');
-
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
 }
-
-
