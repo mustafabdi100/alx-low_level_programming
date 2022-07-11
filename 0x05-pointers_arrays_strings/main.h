368b541fddf9b8dd9b057b8294de4201b226a85a
mustafabdi100 <mustafa300abdi@gmail.com>
2022-07-11 05:24:27 -0700

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,22 +1,32 @@
-#ifndef MAIN.H
+#ifndef MAIN_H
 
-#define MAIN.H
+#define MAIN_H
+
+#include <stdio.h>
 
 
 
-int _putchar(char c);
+/**
+ *
+ * * void prototypes()
+ *
+ * *char prototypes()
+ *
+ * * int prototypes()
+ *
+ * */
 
 void reset_to_98(int *n);
-
-void swap_int(int *a, int *b);
-
-int _strlen(char *s);
 
 void _puts(char *str);
 
 void print_rev(char *s);
 
 void rev_string(char *s);
+
+void swap_int(int *a, int *b);
+
+void swap_char(char *a, char *b);
 
 void puts2(char *str);
 
@@ -26,8 +36,12 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+int _putchar(char c);
+
+int _strlen(char *s);
+
 int _atoi(char *s);
 
+#endif /*MAIN_H*/
 
-
-#endif /* MAIN.H */
+Footer
