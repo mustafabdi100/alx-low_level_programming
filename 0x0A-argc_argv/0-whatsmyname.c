0481b08836321358a0273a12252305ea41ba5f3a
mustafabdi100 <mustafa300abdi@gmail.com>
2022-07-26 13:43:05 +0300

    s

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,42 +1,14 @@
-#include "holberton.h"
 #include <stdio.h>
 
-void print_string(char *str);
-
 /**
- * main - Prints its name
- *
- * @argc: Length of argv
- *
- * @argv: Arrays of strings of the program arguments
- *
- * Return: 0, Success
- */
-
-int main(int argc __attribute__((unused)), char *argv[])
+* main - prints program name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	print_string(argv[0]);
-	putchar('\n');
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
-
-/**
- * print_string - Prints all char of a string
- *
- * @str: Pointer to string
- *
- * Return: void
- */
-
-void print_string(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		i++;
-	}
-}
-Footer
